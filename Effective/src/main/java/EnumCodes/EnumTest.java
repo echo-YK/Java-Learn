b460fb728a93e71087f3adf56d90d48031df6211
echo <531396254@qq.com>
2023-07-24 23:10:54 +0800

    EffectiveJAVA 1、全局加入readme 2、枚举学习的代码和感悟

---
+++ Effective/src/main/java/EnumCodes/EnumTest.java
@@ -0,0 +1,41 @@
+package EnumCodes;
+
+/**
+ * 枚举类
+ */
+public class EnumTest {
+    public static void main(String[] args) {
+        // 枚举类中的变量本身可以通过values方法进行遍历，values方法以变量声明的顺序返回变量
+        for(Fruit f : Fruit.values()) {
+            // 枚举变量本身可以以字符串方式进行输出
+            System.out.printf("fruit: %s count total price %d \n",f, f.getTotalPrice());
+        }
+    }
+
+    public enum Fruit {
+        APPLE(3, 4),
+        CHERRY(10, 3),
+        PEACH(4,10);
+
+        private final int price;
+        private final int num;
+
+        private final int total;
+
+        /**
+         * 本构造方法不对外开发，而是在上述变量定义时进行自动实例化变量
+         * @param price
+         * @param num
+         */
+        Fruit(int price, int num) {
+            this.price = price;
+            this.num = num;
+            this.total = price * num;
+        }
+
+        public int getTotalPrice() {
+            return this.total;
+        }
+    }
+
+}
---
+++ Effective/src/main/java/EnumCodes/EnumWithMethod.java
@@ -0,0 +1,71 @@
+package EnumCodes;
+
+/**
+ * 1、重写toString方法，自定义输出方式
+ * 2、使用abstract方法，强制每个枚举类必须实现指定方法
+ */
+public class EnumWithMethod {
+    public static void main(String[] args) {
+        for(Operations p : Operations.values()) {
+            System.out.printf("%d %s %d = %d\n", 2, p, 3, p.apply(2,3));
+        }
+    }
+
+    private enum Operations {
+        ADD("+") {
+            public int apply(int a, int b) {
+                return a + b;
+            }
+        },
+        SUB("-") {
+            public int apply(int a, int b) {
+                return a - b;
+            }
+        },
+        TIMES("*") {
+            public int apply(int a, int b) {
+                return a * b;
+            }
+        },
+
+        DIVDE("/") {
+            public int apply(int a, int b) {
+                return a / b;
+            }
+        };
+        //symbol变量就是enum中每个变量构造时的变量，如ADD中的"+"
+        private final  String symbol;
+
+        /**
+         * 枚举类构造方法，
+         * @param symbol
+         */
+        Operations(String symbol) {
+            this.symbol = symbol;
+        }
+
+        /**
+         * 抽象方法，限制每个枚举类必须实现apply
+         * @param a
+         * @param b
+         * @return
+         */
+        public abstract int apply(int a, int b);
+
+        /**
+         * 重写枚举的toString方法，实现打印的不是“ADD”，而是“+”
+         * 如果注释掉，效果就是：
+         * 2 ADD 3 = 5
+         * 2 SUB 3 = -1
+         * 2 TIMES 3 = 6
+         * 2 DIVDE 3 = 0
+         * @return
+         */
+        @Override
+        public String toString() {
+            return symbol;
+        }
+    }
+
+
+}
